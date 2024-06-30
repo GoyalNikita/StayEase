79bec82132f7a99c8b5ca24151e2c885da073427
nikitagoyal <goelnikita136@gmail.com>
2024-06-30 14:06:05 +0530

    refactor: removed unused imports

--- src/main/java/com/org/StayEase/configs/OpenAPIConfig/OpenAPI.java
+++ src/main/java/com/org/StayEase/configs/OpenAPIConfig/OpenAPI.java
@@ -1,7 +1,6 @@
 package com.org.StayEase.configs.OpenAPIConfig;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
--- src/main/java/com/org/StayEase/services/UserService.java
+++ src/main/java/com/org/StayEase/services/UserService.java
@@ -1,17 +1,20 @@
 package com.org.StayEase.services;
 
-
 import com.org.StayEase.dto.RegistrationDto;
 import com.org.StayEase.entities.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
     User createUser(RegistrationDto registrationDto);
+
     User getUserByEmail(String email);
+
     User getUserById(Long id);
+
     List<User> getAllUsers();
+
     User updateUser(User user);
+
     void deleteUser(Long userId);
 }
